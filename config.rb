753225f388492b0dfb27825b54d51a26aebe1b9d
taea <igiari@gmail.com>
2013-08-05 06:18:42 +0900

    Compass setting; config.line_comments = false

--- config.rb
+++ config.rb
@@ -7,9 +7,11 @@
 # require 'susy'
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+  # config.output_style = :compact
+  config.line_comments = false
+end
+
 
 ###
 # Page options, layouts, aliases and proxies
